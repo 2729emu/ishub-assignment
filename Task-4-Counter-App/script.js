5fe255869d95ba6df50ae023d1e1f1d80f60cc4e
2729emu <2729eman@gmail.com>
2026-08-29 21:03:18 +0300

    Create script.js

---
+++ Task-4-Counter-App/script.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+const countDisplay = document.getElementById("count");
+
+function increase() {
+    count++;
+    countDisplay.textContent = count;
+}
+
+function decrease() {
+    if (count > 0) {
+        count--;
+    }
+
+    countDisplay.textContent = count;
+}
