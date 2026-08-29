ac5b713886fe112e3d71033ec3f6720fd5fd6ab4
2729emu <2729eman@gmail.com>
2026-08-29 20:47:20 +0300

    Create script.js

---
+++ Task-1-Text-and-Style/script.js
@@ -0,0 +1,9 @@
+const message = document.getElementById("message");
+
+function changeText() {
+    message.textContent = "Hello";
+}
+
+function changeColor() {
+    message.style.color = "red";
+}
